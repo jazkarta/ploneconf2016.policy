7fe40b947ce06eb03cdc234c0e51fd0cf074c081
witekdev <witekdev@gmail.com>
2016-06-19 13:21:45 -0700

    add training class view scafold

--- ploneconf2016/policy/content/training_class.py
+++ ploneconf2016/policy/content/training_class.py
@@ -1,6 +1,10 @@
 from plone.indexer.decorator import indexer
 from plone.supermodel import model
-
+from Products.Five import BrowserView
 
 class ITrainingClass(model.Schema):
     model.load('models/training_class.xml')
+
+
+#class TrainingClassView(BrowserView):
+#    pass
