881e7dd9fee6848bdc2c404b436369d7ec397dd4
witekdev <witekdev@gmail.com>
2016-06-26 18:56:47 -0700

    use TrainingClassView for python logic to parse instructor data - wip

--- ploneconf2016/policy/content/training_class.py
+++ ploneconf2016/policy/content/training_class.py
@@ -1,3 +1,4 @@
+from plone import api
 from plone.indexer.decorator import indexer
 from plone.supermodel import model
 from Products.Five import BrowserView
@@ -6,5 +7,14 @@ class ITrainingClass(model.Schema):
     model.load('models/training_class.xml')
 
 
-#class TrainingClassView(BrowserView):
-#    pass
+class TrainingClassView(BrowserView):
+
+    def instructors(self):
+        """ parse instructor set into something template can iterate over
+        """        
+        instructor_set = self.context.instructor
+        instructors = [x for x in instructor_set]
+        # XXX get instructors out of set        
+
+        return [{'url':'something', 'name' :'P5Nina'},
+                {'url':'something2', 'name' :'P5Nick'}]
