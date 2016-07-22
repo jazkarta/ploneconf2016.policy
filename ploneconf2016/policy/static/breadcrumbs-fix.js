490836452a05cb0f331373507453a559b1b1d805
witekdev <witekdev@gmail.com>
2016-07-22 11:06:00 -0700

    remove breadbrumbs js fix

--- ploneconf2016/policy/static/breadcrumbs-fix.js
+++
@@ -1,9 +0,0 @@
-jQuery(function($) {
-    $().ready(function() {
-
-        // hide breadcrums if only "Home" is being shown (ie. home screen)
-        if ( $('#breadcrumbs-home').siblings().length == 0 ) {
-            $('#breadcrumbs-home').hide()
-        }
-    });
-});
