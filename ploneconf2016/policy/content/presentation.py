27e232f7f6cc4771afaaad04000b81f323fb8139
witekdev <witekdev@gmail.com>
2016-09-19 13:20:39 -0700

    add presentation view, fix 1000-1001 upgrade step and add another, fix html on trainingclass template

--- ploneconf2016/policy/content/presentation.py
+++ ploneconf2016/policy/content/presentation.py
@@ -1,7 +1,41 @@
 from plone import api
 from plone.indexer.decorator import indexer
 from plone.supermodel import model
-
+from Products.Five import BrowserView
+from .vocabularies import PRESENTATION_DURATION_TYPES, LEVEL_TYPES, \
+AUDIENCE_TYPES
 
 class IPresentation(model.Schema):
     model.load('models/presentation.xml')
+
+
+class PresentationView(BrowserView):
+
+    def duration_vocab(self):
+        return PRESENTATION_DURATION_TYPES
+
+    def level_vocab(self):
+        return LEVEL_TYPES
+
+    def audience_vocab(self):
+        return AUDIENCE_TYPES
+
+    def speakers(self):
+        """ parse speaker set into something template can iterate over
+        """        
+        speaker_set = self.context.speaker
+        speakers = [api.content.get(UID=x) for x in speaker_set]
+        speaker_data = []
+        for x in speakers:
+            speaker_data.append({'url':x.absolute_url(), 'name': x.title})
+        return speaker_data
+
+    def vocab_title(self, values, vocab):
+        """ return title when given the vocabulary and the value key
+        """
+        if not isinstance(values, (list, set)):
+            result = vocab.getTerm(values).title
+            if vocab == AUDIENCE_TYPES:
+                return [result] # hacky fix
+            return result
+        return [vocab.getTerm(x).title for x in values ]
